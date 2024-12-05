ef7078b972b957dea39a24eb0cfab0de941c8288
felfire <fel0fire@mail.ru>
2024-12-05 21:30:59 +0300

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x < 5 ? "greater" : "lesser");
+        System.out.println(Arrays.toString(new int[] {1, 2, 3, 4, 5}));
+    }
+}
+
