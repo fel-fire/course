fae7baa6edfaf7c267b67b7feb2fd0123f81fb66
felfire <fel0fire@mail.ru>
2024-12-06 21:27:09 +0300

    Update Block4.java, Main.java

--- src/Block4.java
+++ src/Block4.java
@@ -1,2 +1,113 @@
 public class Block4 {
+
+    //4.1
+    public static String listNums(int x) {
+        String out = "";
+        for (int i = 0; i <= x; i++) {
+            out += i + " ";
+        }
+        return out;
+    }
+
+    //4.2
+    public static String reverseListNums(int x) {
+        String out = "";
+        for (int i = x; i >= 0; i--) {
+            out += i + " ";
+        }
+        return out;
+    }
+
+    //4.3
+    public static String chet(int x) {
+        String out = "";
+        for (int i = 0; i <= x; i += 2) {
+            out += i + " ";
+        }
+        return out;
+    }
+
+    //4.4
+    public static int pow(int x, int y) {
+        if (x == 0 || y < 0) return 0;         //для возведения в отрицательную степень метод не реализован )
+
+        int result = 1;
+        for (int i = 1; i <= y; i++) {
+            result *= x;
+        }
+        return result;
+    }
+
+    //4.5
+    public static int numLen(long x) {
+        if (x == 0) return 1;
+
+        int count = 0;
+        while (x != 0) {
+            x /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    //4.6
+    public static boolean equalNum(int x) {
+        int lastNumber = x % 10;
+        while ((x /= 10) != 0) {
+            if (x % 10 != lastNumber) return false;
+        }
+        return true;
+    }
+
+    //4.7
+    public static void square(int x) {
+        if (x <= 0) return;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    //4.8
+    public static void leftTriangle(int x) {
+        if (x <= 0) return;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    //4.9
+    public static void rightTriangle(int x) {
+        if (x <= 0) return;
+
+        for (int i = 1; i <= x; i++) {
+            System.out.print(" ".repeat(x - i) + "*".repeat(i));
+            System.out.println();
+        }
+    }
+
+    //4.10
+    public static void guessGame() {
+        int randomNum = 3;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("What number am I thinking (0 to 9)? :");
+
+        while (true) {
+            int x = sc.nextInt();
+            if (x != randomNum) {
+                System.out.println("No, try again");
+            } else {
+                System.out.println("Yes, it`s " + randomNum);
+                break;
+            }
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-
+//
 //        System.out.println(Block2.fraction(5.25));
 //        System.out.println(Block2.sumLastNums(-4568));
 //        System.out.println(Block2.charToNum('9'));
@@ -25,8 +25,19 @@ public class Main {
 //        System.out.println(Block3.magic6(1, 6));
 //        System.out.println(Block3.age(25));
 //        System.out.println(Block3.day(0));
-        Block3.printDays("четверг");
-
+//        Block3.printDays("четверг");
+//
+//        System.out.println();
+//        System.out.println(Block4.listNums(15));
+//        System.out.println(Block4.reverseListNums(15));
+//        System.out.println(Block4.chet(15));
+        System.out.println(Block4.pow(-4, 3));
+//        System.out.println(Block4.numLen(12345));
+//        System.out.println(Block4.equalNum(-11));
+//        Block4.square(9);
+//        Block4.leftTriangle(9);
+//        Block4.rightTriangle(9);
+//        Block4.guessGame();
 
     }
 }
